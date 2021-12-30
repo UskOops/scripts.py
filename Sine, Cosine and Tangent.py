3bb4b7d0154db37a1cbba85c641b2ac5689f8def
Marco A. L. Dias <marcolopesdias@outlook.com>
2021-12-30 15:13:22 -0300

    Update Sine, Cosine and Tangent.py

--- Sine, Cosine and Tangent.py
+++ Sine, Cosine and Tangent.py
@@ -1,2 +1,6 @@
 #using import method
 import math
+an = float(input("Type the angle: "))
+print(f"The sine of the angle is: ", math.sin(math.radians(an)))
+print(f"The cosine of the angle is: ", math.cos(math.radians(an)))
+print(f"The tangent of the angle is: ", math.tan(math.radians(an)))
