15c5bbc22b9ddc1e80b0eb765ad14a1c6e31e90d
Marco A. L. Dias <marcolopesdias@outlook.com>
2022-01-04 15:44:22 -0300

    text analysis.py
    texto que analisa e trás alguns resultados sobre seu nome, exemplo:
    caso upper case, lower case...

---
+++ text analysis.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ").strip() #strip() remove espaços em branco
+print('analyzing your name...')
+print(f'your name in upper case is:', name.upper())
+print(f'your name in lower case is:', name.lower())
+print(f'your name has {len(name)} characters') #len() conta o número de caracteres
+print(f'your first name has: letters', name.find(' ')) #find() procura o primeiro caracter
+
+
+
+
