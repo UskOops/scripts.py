06e41d839f231a23739aa7cba2cf0f9d581ea747
Marco A. L. Dias <marcolopesdias@outlook.com>
2022-01-05 15:50:23 -0300

    separating digits.py
    separando digitos por casas

---
+++ separating digits.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+u = num // 1 % 10 #usando modulos
+t = num // 10 % 10
+h = num // 100 % 10
+tt = num // 1000 % 10
+print(f"analyzing this number {num}")
+print(f"the unity is: {u}") #printando o valor da unidade
+print(f"the ten is : {t}")
+print(f"the hundred is: {h}")
+print(f"the thousand is: {tt}")
+
+
+
