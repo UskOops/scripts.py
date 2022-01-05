5332c21d137d3a9b28738c4a7070a84dcbd375e2
Marco A. L. Dias <marcolopesdias@outlook.com>
2022-01-05 16:15:17 -0300

    check the first letter.py
    esse programa checa a primeira letra e gera um resultado true or false

---
+++ checked the first letter.py
@@ -0,0 +1,5 @@
+city = input("where city do you born? ").strip() # strip() usado para remover espaços
+#if city[0] in ['a', 'e', 'i', 'o', 'u']: #laço de repetição
+print(city[0:7].upper() == 'VIÇOSA') # upper() para deixar em maiúsculo
+
+    
