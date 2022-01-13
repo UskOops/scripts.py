524c64d21c88704f16de0d873e1d6fd7cb400cd0
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-01-13 11:10:30 -0300

    using conditions1.py
    using conditions if and else

---
+++ using conditions.py
@@ -0,0 +1,10 @@
+time = input('how many years have your car? ')
+time = int(time) #converte uma string para um inteiro
+if time <=3: #se o tempo for menor ou igual a 3 anos
+    print('your car is new')
+else: #se não
+    print('your car is old')
+    print('you need to get a new one')
+
+    
+    
