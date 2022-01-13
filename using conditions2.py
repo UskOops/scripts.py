0f5ca7db58ef9077362ddaa0618e9a0213687282
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-01-13 11:25:59 -0300

    using conditions2.py
    using simplified conditions

---
+++ using conditions2.py
@@ -0,0 +1,3 @@
+time = int(input('how many years have your car? '))
+print('your car is new') if time <= 3 else print('your car is old') #se o tempo for menor ou igual a 3 anos, printa 'your car is new', senão printa 'your car is old'
+print('you need to get a new one') if time <3 else print('you are good') #se o tempo for menor que 3 anos, printa 'you need to get a new one', senão printa 'you are good'
