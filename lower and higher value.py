d2869e539e546fa415a40201b55be7583dc6d6b6
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-01-19 12:11:12 -0300

    Lower and higher value.py
    Programa que lê o maior e menor número

---
+++ lower and higher value.py
@@ -0,0 +1,11 @@
+first = int(input('type the first number :')) 
+second = int(input('type hte second number :'))
+third = int(input('type the third number :'))
+numbers = [first, second, third] #lista de numeros
+
+print('O maior valor digitado foi {}'.format(max(numbers))) #max() retorna o maior valor da lista
+print('O menor numero digitado foi {}'.format(min(numbers))) #min() retorna o menor valor da lista
+    
+
+
+
