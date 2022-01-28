9977fb91201a823ab31aa38e96c8073a43d161f4
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-01-28 12:28:41 -0300

    average with conditional.py
    média do aluno usando condicionais

---
+++ mundo 2/average with conditional.py
@@ -0,0 +1,13 @@
+from numpy import average
+
+
+n1 = float(input("Enter a note average: "))
+n2 = float(input("Enter a note average: "))
+average = (n1 + n2) / 2
+print("The average is: ", average)
+if average >= 7:
+    print("The student is approved")
+elif average < 7 and average >= 5:
+    print("The student is approved with a warning")
+else:
+    print("The student is disapproved")
