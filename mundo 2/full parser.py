5bc209d7a4102697e2665e41fa7c82d0825ffef2
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-02-14 11:32:29 -0300

    full parser.py
    update code

--- mundo 2/full parser.py
+++ mundo 2/full parser.py
@@ -1,6 +1,3 @@
-from numpy import average
-
-
 sumage = 0 #soma idade
 averageage = 0 #media idade
 bigageH = 0 #maior idade homem
@@ -15,13 +12,13 @@ for p in range(1,5): #numero de pessoas
 if p == 1 and sex in 'Mm': # se a pessoa for a primeira e o sexo for masculino
     bigageH = age
     nameold = name
-if sex in 'Mm' and age > bigageH:
+if sex in 'Mm' and age < bigageH:
     bigageH = name
     nameold = name
-if sex in 'Ff' and age > 25:
+if sex in 'Ff' and age > 25: 
     totwoman25 += 1
 averageage = sumage / 4
 print('The average of age in group is {}'.format(averageage))
-#print('The most old man have {} years e your name is {} '.format(bigageH, nameold))
-print('In this case have {} womans with less 20 age'.format(totwoman25))
+print('In this case have {} womans with most 20 age'.format(totwoman25))
+print('The oldest person is {} with {} years old'.format(nameold, bigageH))
 
