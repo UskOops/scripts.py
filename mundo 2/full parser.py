06100039587bee331432fad4dcebe18e9b2b4f0e
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-02-14 11:45:21 -0300

    full parser.py
    aprimorando o codigo

--- mundo 2/full parser.py
+++ mundo 2/full parser.py
@@ -1,24 +1,28 @@
-sumage = 0 #soma idade
-averageage = 0 #media idade
-bigageH = 0 #maior idade homem
-nameold = '' #nome da pessoa mais velha
-totwoman25 = 0 #total de mulheres -25 anos
+#lista onde passa as inf das pessoas
+list_name = []
+list_name_man = []
+list_age = []
+list_sex = []
+list_age_man = []
+list_age_woman = []
 
-for p in range(1,5): #numero de pessoas
-    name = input('Type the NAME of the person: ')
-    age = int(input('Type the AGE of the person: '))
-    sex = (input('Sex [M/F]: ')).strip()
-    sumage += age
-if p == 1 and sex in 'Mm': # se a pessoa for a primeira e o sexo for masculino
-    bigageH = age
-    nameold = name
-if sex in 'Mm' and age < bigageH:
-    bigageH = name
-    nameold = name
-if sex in 'Ff' and age > 25: 
-    totwoman25 += 1
-averageage = sumage / 4
-print('The average of age in group is {}'.format(averageage))
-print('In this case have {} womans with most 20 age'.format(totwoman25))
-print('The oldest person is {} with {} years old'.format(nameold, bigageH))
+for c in range (1,5):
+    print('-------------- {}ª person ----------------'.format(c)) #imprime a lista de nomes dentro do for
+    name = str(input('Type the NAME: ')).strip().upper() #strip remove os espaços em branco e upper converte para maiusculo
+    list_name.append(name) #append adiciona o nome na lista
 
+    age = int(input('Type the AGE: '))
+    list_age.append(age)
+
+    sex = str(input('Type the SEX M/F: ')).strip().upper()
+    list_sex.append(sex)
+
+    if sex == 'M': 
+        list_name_man.append(name)
+        list_age_man.append(age)
+    if sex == 'F' and age < 20:
+        list_age_woman.append(age)
+
+print('The mean age of the group is  {}'.format(sum(list_age)/len(list_age)))
+print('Name of the most old man: {}'.format(list_name_man[list_age_man.index(max(list_age_man))]))
+print('Total of womans with less 20 years old: {}'.format(len(list_age_woman)))
