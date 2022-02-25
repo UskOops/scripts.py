7d21405a5f52ca475ae4cea5e5d1bb62cf2f0865
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-02-25 16:15:09 -0300

    Arithmetic Progression v2.0.py
    using WHILE for PA

---
+++ mundo 2 (using for)/estrutura while/Arithmetic Progression v2.0.py
@@ -0,0 +1,10 @@
+print('PA Generator')
+print('-='*15)
+first = int(input("Enter the first term of the AP: "))
+reason = int (input('Reason PA: '))
+term = first
+cont = 1
+while cont <= 10:
+    print('{} → '.format(term), end=' ')
+    term += reason
+    cont += 1
