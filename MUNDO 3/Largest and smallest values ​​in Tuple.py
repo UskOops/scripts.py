a6a24c35929b5e2cf32dd796796ed51276eaf16a
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-03-23 16:03:50 -0300

    Largest and smallest values ​​in Tuple.py
    listando valores usando tuplas

---
+++ MUNDO 3/Largest and smallest values ​​in Tuple.py
@@ -0,0 +1,7 @@
+from random import sample #sample() function returns a list of random elements from the original list.
+n = (tuple(sample(range(1, 10), 5))) #tuple() function converts a list into a tuple.
+print(n)
+print('-=' * 15)
+print(f'The largest value in the tuple is {max(n)} and the smallest value is {min(n)}')
+print('-=' * 15)
+print(f'The first value is {n[0]} and the last value is {n[-1]}')
