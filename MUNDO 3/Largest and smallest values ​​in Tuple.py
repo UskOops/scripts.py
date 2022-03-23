689d20fe395d7f43ed44220e636a71191a665179
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-03-23 16:06:36 -0300

    Update Largest and smallest values ​​in Tuple.py

--- MUNDO 3/Largest and smallest values ​​in Tuple.py
+++ MUNDO 3/Largest and smallest values ​​in Tuple.py
@@ -3,5 +3,5 @@ n = (tuple(sample(range(1, 10), 5))) #tuple() function converts a list into a tu
 print(n)
 print('-=' * 15)
 print(f'The largest value in the tuple is {max(n)} and the smallest value is {min(n)}')
-print('-=' * 15)
+print('-=' * 15) 
 print(f'The first value is {n[0]} and the last value is {n[-1]}')
