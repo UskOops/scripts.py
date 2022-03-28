dc9263e803255b1b437d46094cba098e7538a971
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-03-28 16:53:18 -0300

    Extracting data from a List.py
    extraindo dados de uma lista usando metodo "reverse=True"

---
+++ MUNDO 3/Extracting data from a List.py
@@ -0,0 +1,13 @@
+values = []
+while True:
+    values.append(int(input('Type a value: ')))
+    answer = (input('Do you want to continue? [Y/N] ')).strip().upper()[0]
+    if answer in 'Nn':
+        break
+print(f'You typed {len(values)} values.')
+values.sort(reverse=True) #ordena a lista de forma decrescente
+print(f'The values typed in order are: {values}')
+if 5 in values:
+    print('The value 5 appears in the list.')
+else:
+    print('The value 5 does not appear in the list.')
