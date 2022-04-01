d78970e99bfeebad7ee6a686c8b6bc9dde94e239
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-01 16:49:25 -0300

    Update greater and lesser weight.py

--- MUNDO 3/greater and lesser weight.py
+++ MUNDO 3/greater and lesser weight.py
@@ -2,8 +2,8 @@ name = []
 weight = []
 more = less = 0
 while True:
-    name.append(input("Enter name: "))
-    name.append(float(input("Enter weight: ")))
+    name.append(input("Enter NAME: "))
+    name.append(float(input("Enter WEIGHT: ")))
     if len (weight) == 0:
         more = less = name[1]
     elif name[1] > more:
@@ -13,17 +13,17 @@ while True:
 
     weight.append(name[:])
     name.clear()
-    answer = input("Do you want to continue? (y/n) ")
+    answer = input("Do you want to continue? (Y/N) ")
     if answer in "nN":
         break
 print('-' * 15)
-print(f'In total you cadastred {len(weight)} people')
-print(f'The heavier is {more}')
+print(f'In total you cadastred: {len(weight)} people')
+print(f'THE HEAVIER PERSON IS: {more}')
 for p in weight:
     if p[1] == more:
         print(f'{p[0]},', end=' ')
 print()
-print(f'The lighter is {less}')
+print(f'THE LIGHTER PERSON IS: {less}')
 for p in weight:
     if p[1] == less:
         print(f'{p[0]},', end=' ')
