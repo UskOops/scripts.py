ab34b65ca4ddfb06b89ef045ece68f8c72d8d93a
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-06 17:11:56 -0300

    Update Predictions for the Mega Sena.py

--- MUNDO 3/Predictions for the Mega Sena.py
+++ MUNDO 3/Predictions for the Mega Sena.py
@@ -6,27 +6,27 @@ print('='*15)
 print('{:^15}'.format('MEGA SENA'))
 print('='*15)
 qtde = int(input('how many games do you want to make?  '))
-tot = 1
+tot = 1 # variável para o cálculo do total de números sorteados
 while tot <= qtde:
     count = 0
-    while True:
+    while True: 
         num = randint(1, 60)
-        if num not in list:
-            list.append(num)
-            count += 1
-        if count >= 6:
+        if num not in list: # se o número não estiver na lista, ele será adicionado
+            list.append(num) # adiciona o número sorteado na lista
+            count += 1 # conta quantos números foram sorteados
+        if count >= 6:# se o número de números sorteados for igual ou maior que 6, sai do loop
             break
-    list.sort()
-    games.append(list[:])
-    list.clear()
-    tot += 1
+    #list.sort() # ordena a lista (opcional)
+    games.append(list[:]) # adiciona a lista de números sorteados na lista de jogos
+    list.clear() # limpa a lista para que possa receber novos números
+    tot += 1 # conta quantos jogos foram feitos
 print('-'*15)
 print('='*3, f' GENERATED {qtde} GAMES ', '='*3)
 print('loading...')
 sleep(2.5)
 print('-'*15)
-for i, l in enumerate(games):
-    print(f'Game {i+1}: {l}')
+for i, l in enumerate(games): # enumerate para mostrar o número do jogo
+    print(f'Game {i+1}: {l}') # mostra os números sorteados
     sleep(0.5)
 
 
