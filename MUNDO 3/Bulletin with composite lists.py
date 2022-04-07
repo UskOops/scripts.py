11bf455c0305b5c69a10ee13a3502bce36134eb3
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-07 16:39:06 -0300

    Update Bulletin with composite lists.py

--- MUNDO 3/Bulletin with composite lists.py
+++ MUNDO 3/Bulletin with composite lists.py
@@ -12,7 +12,7 @@ while True:
     if answer in "Nn":
         break
 print("-" * 40)
-print("{:<10}{:<8.5}{:<8.5}{:<10}".format("NAME", "RESULT 1", "RESULT 2", "AVERAGE"))
+print("{:<10}{:<10}{:<10}{:<10}".format("NAME", "RESULT 1", "RESULT 2", "AVERAGE"))
 print("-" * 40)
 for i, a in enumerate(token): # i = indice, a = aluno
     print("{:<10}{:<10}{:<10}{:<10}".format(a[0], a[1][0], a[1][1], a[2]))
