85d855166f28ff3033d50e66c803b1bacb558164
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-07 16:49:10 -0300

    Update Bulletin with composite lists.py

--- MUNDO 3/Bulletin with composite lists.py
+++ MUNDO 3/Bulletin with composite lists.py
@@ -1,3 +1,4 @@
+from libcst import While
 from numpy import average
 
 
@@ -16,3 +17,16 @@ print("{:<10}{:<10}{:<10}{:<10}".format("NAME", "RESULT 1", "RESULT 2", "AVERAGE
 print("-" * 40)
 for i, a in enumerate(token): # i = indice, a = aluno
     print("{:<10}{:<10}{:<10}{:<10}".format(a[0], a[1][0], a[1][1], a[2]))
+while True:
+    print("-" * 40)
+    options = int(input('Show results of which student? (type 999 to exit): '))
+    if options == 999:
+        print('WAIT...')
+        break
+    if options <= len(token) - 1:
+       print(f'{token[options][0]} average: {token[options][2]}')
+    else:
+        print('Invalid option')
+print('-' * 40)
+print('END')
+print('-' * 40)
