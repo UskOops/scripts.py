6b21ac1c740ab1209e97b992a336cf96a5ea417a
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-12 14:07:59 -0300

    Update unite dictionary and lists.py

--- MUNDO 3/unite dictionary and lists.py
+++ MUNDO 3/unite dictionary and lists.py
@@ -1,17 +1,18 @@
-people = {}
-guys = []
+people = {} # dicionario vazio
+guys = [] # lista vazia
+sum = average = 0 # soma e media
 while True:
-    people.clear()
+    people.clear() # limpa o dicionario
     people['name'] = input('What is your name? ')
     print('-=' * 20)
-    while True:
+    while True: 
         people['sex'] = input('SEX: [M/F] ').upper()[0]
         print('-=' * 20)
-        if people ['sex'] in 'MF':
+        if people ['sex'] in 'MF': 
             break
         print('ERROR! Please, type you correct sex (M or F): ')
     people ['age'] = int(input('AGE: '))
-    guys.append(people.copy())
+    guys.append(people.copy()) 
     print('-=' * 20)
     while True:
         answer = input('Do you want continue? [Y/N] ').upper()[0]
@@ -20,4 +21,9 @@ while True:
         print('ERROR! Answer only Y or N: ')
     if answer == 'N':
         break
-print(guys)
+print('-=' * 20)
+print(f'Total of people: {len(guys)}')
+for guy in guys:
+    sum += guy['age'] # soma das idades
+    average = sum / len(guys) # media das idades
+print(f'The average of age is {average:.2f}')
