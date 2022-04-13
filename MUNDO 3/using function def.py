036e9254ebd48cee8f74d0bca122c803a9d52bb8
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-13 13:21:03 -0300

    Update using function def.py

--- MUNDO 3/using function def.py
+++ MUNDO 3/using function def.py
@@ -8,7 +8,7 @@
 #sum(70,80)
 #sum(90,100, 110, 120)
 
-def double(list):
+def double(list): #passando parametro como lista
     pos = 0
     while pos < len(list):
         list[pos] *= 2 #lista recebe o valor do elemento da lista vezes 2
