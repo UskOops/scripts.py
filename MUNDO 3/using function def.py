404997debd5b939afb2d5097b921492a995f887f
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-13 13:27:48 -0300

    Update using function def.py
    APRENDO O USO DA FUNÇÃO "DEF"

--- MUNDO 3/using function def.py
+++ MUNDO 3/using function def.py
@@ -1,18 +1,21 @@
-#def sum(a, b ,*num):
-#    s = a + b , num
-#    print(s)
-## main program
-#sum(10,20)
-#sum(30,40)
-#sum(50,60)
-#sum(70,80)
-#sum(90,100, 110, 120)
+def sum(*values):
+    s = 0
+    for num in values: #desempacotar valores
+        s += num
+    print(f'Calculate the sum of {values} we have {s}')
+# main program
+sum(10,20)
+sum(30,40)
+sum(50,60)
+sum(70,80)
+sum(90,100, 110, 120)
 
-def double(list): #passando parametro como lista
-    pos = 0
-    while pos < len(list):
-        list[pos] *= 2 #lista recebe o valor do elemento da lista vezes 2
-        pos += 1
-values = [1,2,3,4,5] #essa lista terá seus valores multiplicados por 2
-double(values)
-print(values)
+#def double(list): #passando parametro como lista
+#    pos = 0
+#    while pos < len(list):
+#        list[pos] *= 2 #lista recebe o valor do elemento da lista vezes 2
+#        pos += 1
+#values = [1,2,3,4,5] #essa lista terá seus valores multiplicados por 2
+#double(values)
+#print(values)
+
