d27800a060800f1ad9bc4344e5038a9a41dd9ce0
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-13 13:53:15 -0300

    Update Function that calculates area.py

--- MUNDO 3/Function that calculates area.py
+++ MUNDO 3/Function that calculates area.py
@@ -1,10 +1,9 @@
-def area(l, w):
-    a = l * w
-    print(f'The area of land is {l}x{w} is {a}m2 ')
+def area(l, w): #função que calcula a área, passo o parametro l e w dentro da função
+    a = l * w #variavel que recebe a multiplicação de l e w (os parametros)
+    print(f'The area of land is {l } x {w} is {a} m2 ')
 #programa principal
 print('---Land Control--')
+l = float(input('Enter the length of the land(m): '))
+w = float(input('Enter the width of the land(m): '))
 print('-' * 20)
-l = float(input('Enter the length of the land: '))
-w = float(input('Enter the width of the land: '))
-print('-' * 20)
-area(l, w)
+area(l, w) #chama a função area e passa os parametros l e w
