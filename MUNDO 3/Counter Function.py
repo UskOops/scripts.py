213f08a4d50dffdbdc9c3726eb7b6587eb6e5680
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-13 15:12:41 -0300

    Update Counter Function.py

--- MUNDO 3/Counter Function.py
+++ MUNDO 3/Counter Function.py
@@ -1,14 +1,21 @@
+from time import sleep as sl
+
 def counter(i, f, s): #i = initial, f = final, p = step
     print(f'Counter the {i} at {f} the {s} in the {s}')
-    print('~'*20)
-    
     if i < f:
         count = i
         while count <= f:
-            print(f'{count}', end=' ')
+            print(f'{count}', end=' ', flush=True)
+            sl(0.8)
             count += s
-    print('END!')
+        print('END!')
+    elif i > f:
+        count = i
+        while count >= f:
+            print(f'{count}', end=' ', flush=True) # comando FLUSH desliga o buffer de tela e faz o sleep carregar proceduralmente
+            sl(0.8)
+            count -= s
+        print('END!')
 counter(0,100,10) #contador de 0 a 100 de 1 em 1
-counter(100,0,-10) #contador de 100 a 0 de -1 em -1
 counter(0,10,2) #contador de 0 a 10 de 2 em 2
 
