123af7a264484e3b1b406b52d47763a5e58a805b
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-25 15:04:09 -0300

    try&exception.py
    trabalhando com erros e exceção

---
+++ MUNDO 3/try&exception.py
@@ -0,0 +1,10 @@
+try: #tenta executar o bloco de código abaixo
+    a = int(input("Enter a number: "))
+    b = int(input("Denominator: "))
+    r = a/b
+except Exception as error: #se ocorrer um erro, executa o bloco abaixo
+    print(f'The problem found is {error.__class__.__name__}')
+else: #se não ocorrer erro, executa o bloco abaixo (tratamento de erro)
+    print(f'The result is {r:.2f}')
+finally: #vai acontecer independente de ocorrer erro ou não
+    print("Back soon!")
