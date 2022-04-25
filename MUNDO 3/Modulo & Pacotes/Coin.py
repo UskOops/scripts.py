d60a099ce5312448d1feef077c397ec71aef692c
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-25 09:46:13 -0300

    Update Coin.py
    improve code for mudule import

--- MUNDO 3/Modulo & Pacotes/Coin.py
+++ MUNDO 3/Modulo & Pacotes/Coin.py
@@ -1,15 +1,22 @@
-def up(price, rate):
+def up(price=0, rate=0, format=False):
     answer =  price + (price * rate/100)
     return answer
 
-def down(price, rate):
+def down(price=0, rate=0, format=False):
     answer = price - (price * rate/100)
-    return answer
+    return answer if format is False else coin(answer, '.2f')
+    
 
-def double(price):
+def double(price=0, format=False):
     answer = price * 2
-    return answer
+    return answer if not format is False else coin(answer, '.2f')
 
-def half(price):
+def half(price=0, format=False):
     answer = price / 2
-    return answer
+    return answer if not format is False else coin(answer, '.2f')
+
+def coin(price=0, coin='R$'):
+    return f'{coin}{price:.2f}'.replace('.', ',')
+
+def resume(price=0, rate=10, format=False):
+    return f'{coin(up(price, rate, format), "")} - {coin(down(price, rate, format), "")}'
