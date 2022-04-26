7f3ecf282a9de2017d1bd59c6fcb45b7bc0dc41f
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-26 10:26:06 -0300

    Update interface.py

--- MUNDO 3/create menu python/interface.py
+++ MUNDO 3/create menu python/interface.py
@@ -1,10 +1,26 @@
+def readint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print("Invalid input, try again")
+            continue
+        except KeyboardInterrupt:
+                print('User cancelled')
+                exit()
+        else:
+            return n
+
+
 def row(size=40):
     return '-' * size
+
 
 def header(txt):
     print(row())
     print(txt.center(40))
     print(row())
+
 
 def menu(list):
     header('MENU PRINCIPAL')
@@ -13,3 +29,5 @@ def menu(list):
         print(f'{c} - {item}')
         c += 1
     print(row())
+    option = readint('Choose an option: ')
+    return option
