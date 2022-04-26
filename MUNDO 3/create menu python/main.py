38617c2fc66482e0d7664ca90b7e857af230ad58
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-26 10:35:00 -0300

    Update main.py

--- MUNDO 3/create menu python/main.py
+++ MUNDO 3/create menu python/main.py
@@ -1,3 +1,20 @@
 from interface import *
+from time import sleep as sl
 
-answer = menu (['Create file', 'Register people', 'List people', 'Exit'])
+while True:
+    answer = menu (['Create file', 'Register people', 'List people', 'Exit'])
+    if answer == 1:
+        print('Creating file')
+    elif answer == 2:
+        print('Registering people')
+    elif answer == 3:
+        print('Listing people')
+    elif answer == 4:
+        print('Exiting...')
+        break
+    else:
+        print('Invalid option')
+    sl(0.5)
+       
+
+        
