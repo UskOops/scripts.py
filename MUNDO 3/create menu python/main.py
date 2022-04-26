6263daa616d7d544064e001fad57d440f1a18e47
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-26 13:34:28 -0300

    Update main.py

--- MUNDO 3/create menu python/main.py
+++ MUNDO 3/create menu python/main.py
@@ -1,15 +1,21 @@
 from interface import *
 from time import sleep as sl
+from archive import *
+
+arch = 'TESTE.txt'
+
+if not archexist(arch):
+    create_archive(arch)
 
 while True:
-    answer = menu (['Create file', 'Register people', 'List people', 'Exit'])
+    answer = menu (['List people', 'Register people', 'Exit'])
     if answer == 1:
-        print('Creating file')
-    elif answer == 2:
         print('Registering people')
-    elif answer == 3:
+        #list content of archive
+        readarchive(arch)
+    elif answer == 2:
         print('Listing people')
-    elif answer == 4:
+    elif answer == 3:
         print('Exiting...')
         break
     else:
