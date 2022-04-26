38468b9f28f078a957a8d2c43e2227730557c757
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-04-26 09:34:00 -0300

    Update try&exception.py

--- MUNDO 3/try&exception.py
+++ MUNDO 3/try&exception.py
@@ -3,7 +3,7 @@ try: #tenta executar o bloco de código abaixo
     b = int(input("Denominator: "))
     r = a/b
 except Exception as error: #se ocorrer um erro, executa o bloco abaixo
-    print(f'The problem found is {error.__class__.__name__}')
+    print(f'The problem found is {error.__class__.__name__}') #printa o nome da classe do erro
 else: #se não ocorrer erro, executa o bloco abaixo (tratamento de erro)
     print(f'The result is {r:.2f}')
 finally: #vai acontecer independente de ocorrer erro ou não
